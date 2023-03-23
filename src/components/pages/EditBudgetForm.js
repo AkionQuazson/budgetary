69f93c4b4c6742dc511607b08cc955b8328c73cf
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2023-03-22 19:37:53 -0600

    working on add/remove subbudgets

--- src/components/pages/EditBudgetForm.js
+++ src/components/pages/EditBudgetForm.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
 
 
 const EditBudgetForm = (props) => {
+    const [subBudgets, setSubBudgets] = useState([]);
+    const {mode} = props
+
+    const addSub = (e) => {
+        e.preventDefault();
+        let tempSubs = [...subBudgets];
+        tempSubs.push({name:'asdf'});
+        setSubBudgets(tempSubs);
+    }
+
+    const deleteBudget = (e) => {
+        e.preventDefault()
+        const id = +e.target.parentNode.id[9];
+    }
+
+    const displaySub = subBudgets.map((sub, i) => {
+        return <li id={'subBudget' + i} key={i}>{sub.name}<button className="deleteButton" onClick={(e) => {deleteBudget(e)}}>X</button></li>
+    })
+    
     return <form>
         <h2>New Budget</h2>
-        <input type="text" />
+        <input type="text" id="budgetName" />
+        <div className="half">
+            <input type="number" id="maxValue" />
+            <input type="color" id="color" />
+        </div>
+        <ul className="half" >
+            {displaySub}
+            <li><input type="text" /><button onClick={(e) => {addSub(e)}}>+</button></li> 
+        </ul>
     </form>
 }
 
