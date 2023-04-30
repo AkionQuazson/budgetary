ed2771d833a62387ed0ed9738009128077fe64c3
AkionQuazson <commodore64archonu@gmail.com>
2023-04-29 18:15:15 -0600

    templated controllers

---
+++ server/controllers/budgets.js
@@ -0,0 +1,14 @@
+const getBudgets = () => {};
+
+const addBudget = () => {};
+
+const editBudget = () => {};
+
+const deleteBudget = () => {};
+
+module.exports = {
+    getBudgets,
+    addBudget,
+    editBudget,
+    deleteBudget
+};
---
+++ server/controllers/transactions.js
@@ -0,0 +1,14 @@
+const getTransactions = () => {};
+
+const addTransaction = () => {};
+
+const editTransaction = () => {};
+
+const deleteTransaction = () => {};
+
+module.exports = {
+    getTransactions,
+    addTransaction,
+    editTransaction,
+    deleteTransaction
+};
