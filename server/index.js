5403389211929880f43754304b49fa4849f502a3
AkionQuazson <commodore64archonu@gmail.com>
2023-05-01 11:04:13 -0600

    connected controllers

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ const {SubBudget} = require('./models/sub_budget')
 const {Transaction} = require('./models/transaction');
 
 const { login, register } = require('./controllers/auth');
+const {setIncome, getBudgets, addBudget, editBudget, deleteBudget} = require('./controllers/budgets')
+const {getTransactions, addTransaction, editTransaction, deleteTransaction} = require('./controllers/transactions')
 
 const app = express();
 
@@ -26,18 +28,18 @@ Budget.hasMany(Transaction);
 SubBudget.hasMany(Transaction);
 
 app.post('/register', register);
-app.post('/login',  login);
-app.post('/income',  ()=>console.log('setIncome'));
+app.post('/login', login);
+app.post('/income', setIncome);
 
-app.get('/budgets',  ()=>console.log('getBudgets'));
-app.post('/budgets',  ()=>console.log('addBudget'));
-app.put('/budgets/:id',  ()=>console.log('editBudget'));
-app.delete('/budgets/:id',  ()=>console.log('deleteBudget'));
+app.get('/budgets', getBudgets);
+app.post('/budgets', addBudget);
+app.put('/budgets/:id', editBudget);
+app.delete('/budgets/:id', deleteBudget);
 
-app.get('/transactions',  ()=>console.log('getTransactions'));
-app.post('/transactions',  ()=>console.log('addTransaction'));
-app.put('/transactions/:id',  ()=>console.log('editTransaction'));
-app.delete('/transactions/:id',  ()=>console.log('deleteTransaction'));
+app.get('/transactions', getTransactions);
+app.post('/transactions', addTransaction);
+app.put('/transactions/:id', editTransaction);
+app.delete('/transactions/:id', deleteTransaction);
 
 sequelize.sync()
 .then(() => {
